218a5a4b488fd7fdcc016e3c98362e08e33ea6e8
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2022-11-10 07:33:45 -0300

    using awawait/async and try/catch

--- src/index.js
+++ src/index.js
@@ -14,14 +14,10 @@ let db;
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-mongoClient
-  .connect()
-  .then(() => {
-    db = mongoClient.db("batePapoUol");
-  })
-  .catch((err) => console.log(err));
+await mongoClient.connect();
+db = mongoClient.db("batePapoUol");
 
-server.post("/participants", (req, res) => {
+server.post("/participants", async (req, res) => {
   /*  const schema = Joi.object({
         name: Joi.string().required(),
     }) */
@@ -30,81 +26,81 @@ server.post("/participants", (req, res) => {
     res.sendStatus(422);
     return;
   }
-  db.collection("participants")
-    .findOne({ name })
-    .then((user) => {
-      if (user !== null) {
-        res.sendStatus(409);
-        return;
-      }
-      db.collection("participants").insertOne({
-        name,
-        lastStatus: Date.now(),
-      });
-      db.collection("messages").insertOne({
-        from: name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: dayjs().format("HH:mm:ss"),
-      });
-      res.sendStatus(201);
-    })
-    .catch((err) => console.log(err));
+  try {
+    const user = await db.collection("participants").findOne({ name });
+
+    if (user !== null) {
+      res.sendStatus(409);
+      return;
+    }
+    await db.collection("participants").insertOne({
+      name,
+      lastStatus: Date.now(),
+    });
+    await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-server.get("/participants", (req, res) => {
-  db.collection("participants")
-    .find()
-    .toArray()
-    .then((participants) => {
-      res.send(participants);
-    })
-    .catch((err) => console.log(err));
+server.get("/participants", async (req, res) => {
+  try {
+    const participants = await db.collection("participants").find().toArray();
+    res.send(participants);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-server.post("/messages", (req, res) => {
+server.post("/messages", async (req, res) => {
   /*  const schema = Joi.object({
         name: Joi.string().required(),
     }) */
   const { to, text, type } = req.body;
   const from = req.headers.user;
-  db.collection("participants")
-    .findOne({ name: from })
-    .then((user) => {
-      if (user === null) {
-        res.sendStatus(422);
-        return;
-      }
-      db.collection("messages").insertOne({
-        from,
-        to,
-        text,
-        type,
-        time: dayjs().format("HH:mm:ss"),
-      });
-      res.sendStatus(201);
-    })
-    .catch((err) => console.log(err));
+  try {
+    const user = await db.collection("participants").findOne({ name: from });
+
+    if (user === null) {
+      res.sendStatus(422);
+      return;
+    }
+    await db.collection("messages").insertOne({
+      from,
+      to,
+      text,
+      type,
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-server.get("/messages", (req, res) => {
+server.get("/messages", async (req, res) => {
   const { limit } = req.query;
   const user = req.headers.user;
-  db.collection("messages")
-    .find()
-    .toArray()
-    .then((messages) => {
-      const filteredMessages = messages.filter(
-        (message) =>
-          message.type === "message" ||
-          (message.type === "private_message" &&
-            (message.to === user || message.from === user))
-      );
-      if (limit) res.send(filteredMessages.slice(-limit));
-      else res.send(filteredMessages);
-    })
-    .catch((err) => console.log(err));
+  try {
+    const messages = await db.collection("messages").find().toArray();
+    const filteredMessages = messages.filter(
+      (message) =>
+        message.type === "message" ||
+        (message.type === "private_message" &&
+          (message.to === user || message.from === user))
+    );
+    if (limit) res.send(filteredMessages.slice(-limit));
+    else res.send(filteredMessages);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 server.listen("5000", () => {
